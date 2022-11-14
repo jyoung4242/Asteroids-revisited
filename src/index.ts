601fdeeada68c4489e08d4031dbf146d42b6c12a
Justin Young <justin_dean_young@yahoo.com>
2022-11-14 12:30:48 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { UI } from "peasy-ui";
 let gameDiv: HTMLElement;
 
 let template = `
-<div id="game" class="gameContainer" style="--screen_width: \${screenwidth}px; --screen_height: \${screenheight}px;">
+<div id="game" class="gameContainer">
 </div>`;
 
 /**
@@ -56,5 +56,17 @@ const init = async () => {
 
 window.addEventListener("load", init, false);
 window.addEventListener("resize", resizeScreen, false);
+/* 
+var orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
+
+if (orientation === "landscape-primary") {
+  console.log("That looks good.");
+} else if (orientation === "landscape-secondary") {
+  console.log("Mmmh... the screen is upside down!");
+} else if (orientation === "portrait-secondary" || orientation === "portrait-primary") {
+  console.log("Mmmh... you should rotate your device to landscape");
+} else if (orientation === undefined) {
+  console.log("The orientation API isn't supported in this browser :(");
+} */
 
 //TODO add in touch listener to enable touch controls
